fe2cf4672ec1737367cc7c65baa4f602b347e41e
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2024-02-05 16:01:45 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -250,7 +250,7 @@ const Home: NextPage = () => {
                             {lang[getLanguagePreference()].section4Description}
                         </p>
                         <a
-                            href="#"
+                            href="/login"
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                         >
                             {lang[getLanguagePreference()].section4Button}
