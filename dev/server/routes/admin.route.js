7f567e1acfa92cffecde85a0cfc86cf9386aeb0b
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2024-02-18 01:46:27 +0700

    fixed error in building scss

--- dev/server/routes/admin.route.js
+++ dev/server/routes/admin.route.js
@@ -6,24 +6,24 @@ const deta = Deta(process.env.SPACE_DETA_PROJECT_KEY);
 const db = deta.Base("Admin");
 
 async function userIsAdmin(email) {
-  let admin = await db.get("email");
-  admin = admin?.value;
-  if (admin?.length > 0) return admin.includes(email);
-  else return false;
+    let admin = await db.get("email");
+    admin = admin?.value;
+    if (admin?.length > 0) return admin.includes(email);
+    else return false;
 }
 
 router.get("/:email", async (req, res) => {
-  return res.status(200).send(
-    JSON.stringify(
-      {
-        statusCode: 200,
-        email: req.params.email,
-        isAdmin: await userIsAdmin(req.params.email),
-      },
-      null,
-      2
-    )
-  );
+    return res.status(200).send(
+        JSON.stringify(
+            {
+                statusCode: 200,
+                email: req.params.email,
+                isAdmin: await userIsAdmin(req.params.email),
+            },
+            null,
+            2
+        )
+    );
 });
 
 module.exports = router;
